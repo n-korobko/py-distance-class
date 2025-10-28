0dc65b71add9426aa380dc4ceb058161f3cf75a8
Nikita <hkorobko@gmail.com>
2025-10-28 18:42:19 +0200

    Fix Distance class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 class Distance:
     def __init__(self, km: float) -> None:
         self.km: float = km
@@ -8,7 +9,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance | int | float") -> "Distance":
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
